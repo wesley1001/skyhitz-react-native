b48837458611aec83e599de0c4d8574c5e008472
Alejandro Mendoza <alejandro@skyhitzmusic.com>
2015-10-06 22:30:24 -0500

    clean staging

--- app/config/env.js
+++ app/config/env.js
@@ -2,11 +2,7 @@
 'use strict';
 
 var Env = {
-
-    FB_URL: __DEV__ ? 'https://skyhitz-staging.firebaseio.com/sandbox/alejo/v1' : 'https://skyhitz-prod.firebaseio.com/sandbox/alejo/v1',
-    S3_URL: __DEV__ ? 'https://skyhitz-staging.s3.amazonaws.com' : 'https://skyhitz-prod.s3.amazonaws.com',
-    S3_BUCKETNAME: __DEV__ ? 'skyhitz-staging' : 'skyhitz-prod'
-
+    FB_URL: __DEV__ ? 'https://skyhitz-staging.firebaseio.com/sandbox/staging/v1' : 'https://skyhitz-prod.firebaseio.com/sandbox/prod/v1'
 };
 
 module.exports = Env;
